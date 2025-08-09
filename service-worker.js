0d9acea61af833d4a617d2e478bb5240bc48174a
nishikawayota <nishi01001128@gmail.com>
2025-08-09 16:17:39 +0900

    音声追加処理

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = 'seat-app-cache-v1';
+const CACHE_NAME = 'seat-app-cache-v3';
 const urlsToCache = [
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './sounds/drumroll.mp3',
+  './sounds/fanfare.mp3' // 追加
 ];
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
   );
 });
 
